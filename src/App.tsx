31a26ccf3dd8ae4511de3abb9630362d1f3d2e8a
mostafa mohammed <000madz000@gmail.com>
2024-05-09 23:13:25 +0300

    make the app direction responsive with languages

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,24 @@ import AutoHashrouterRedirector from './auto-hashrouter-redirector';
 import LoginPage from './pages/login';
 import DbCredentialsPage from './pages/db-credentials';
 import HomePage from './pages/home';
+import CardsPage from './pages/cards-page';
+import { useIntl } from 'react-intl';
 
 function App() {
+  const intl = useIntl();
+
   return (
-    <HashRouter>
+    <HashRouter >
       <title>Commission Points</title>
       <AutoHashrouterRedirector />
-      <Routes>
-        <Route path='/' element={<LoginPage />} />
-        <Route path='/home' element={<HomePage />} />
-        <Route path='/db-credentials' element={<DbCredentialsPage />} />
-      </Routes>
+      <div className="App" dir={intl.locale === "ar" ? "rtl" : "ltr"}>
+        <Routes>
+          <Route path='/' element={<LoginPage />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/db-credentials' element={<DbCredentialsPage />} />
+          <Route path='/cards/:cardId' element={<CardsPage />} />
+        </Routes>
+      </div>
     </HashRouter>
   );
 }
