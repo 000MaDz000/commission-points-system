55fbf4e50eb8ba40d79102703f9904913e2dd117
mostafa mohammed <000madz000@gmail.com>
2024-05-09 23:14:35 +0300

    make a radar waves effect

--- src/components/rfid-waiter.tsx
+++ src/components/rfid-waiter.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from "react";
 import useRfid from "../hooks/use-rfid";
 import RadarWaves from "./radar-waves";
+import { useNavigate } from "react-router";
 
 export default function RfidWaiter() {
     const rfid = useRfid();
+    const navigate = useNavigate();
 
-    console.log(rfid);
+    useEffect(() => {
+        if (rfid) {
+            navigate("/cards/" + rfid)
+        }
+    }, [rfid, navigate]);
 
     return (
         <div className="grow relative flex border items-center justify-center overflow-hidden">
