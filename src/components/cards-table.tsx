a0ec0926d936145f907ec87fc23dbff4b0bd9993
mostafa mohammed <000madz000@gmail.com>
2024-06-06 13:53:33 +0300

    fecth data onscroll to the bottom of table

--- src/components/cards-table.tsx
+++ src/components/cards-table.tsx
@@ -1,13 +1,12 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import Card, { CardType } from "../models/card";
 import { ObjectId } from "bson";
-import { IconButton, Paper, Table, TableBody, TableCell, TableFooter, TableHead, TableRow, Tooltip } from "@mui/material";
+import { Paper, Table, TableBody, TableCell, TableFooter, TableHead, TableRow, Tooltip } from "@mui/material";
 import useTranslation from "../hooks/useTranslation";
 import { PersonType } from "../models/person";
 import { Peaple } from "./peaple-table";
 import { Types } from "mongoose";
 import CardRow from "./card-row";
-import { HdrPlus, PlusOne } from "@mui/icons-material";
 import AddCardButton from "./add-card-button";
 
 
@@ -17,28 +16,55 @@ export type CardsType = OneCardsType[];
 export default function CardsTable() {
     const [cards, setCards] = useState<CardsType>([]);
     const { t } = useTranslation();
+    const ref = useRef<HTMLDivElement>(null);
+    const hiddenRef = useRef<HTMLDivElement>(null);
+
+    const fetchCards = async () => {
+        if (cards.length) {
+
+            // const fetchedCards = await Card.collection.aggregate([
+            //     {
+            //         "$sort": {
+            //             _id: 1
+            //         }
+            //     },
+            //     {
+            //         $match: {
+            //             _id: {
+            //                 $gt: new ObjectId(cards[cards.length - 1]._id.toString()), // id of last fetched document
+            //             }
+            //         }
+            //     },
+            //     {
+            //         $limit: 20
+            //     },
+            //     {
+            //         $lookup: {
+            //             from: "peaple",
+            //             localField: "person",
+            //             foreignField: "_id",
+            //             as: "PP"
+            //         }
+            //     },
+            //     // {
+            //     //     $set: {
+            //     //         person: { $arrayElemAt: ["$person", 0] },
+            //     //     }
+            //     // }
+            // ]);
+
+            return Card.find({ addedAt: { $gt: cards[cards.length - 1].addedAt } }).sort({ addedAt: 1 }).limit(20).populate("person") as unknown as CardsType;
+
+        }
+        else {
+            const fetchedCards = await Card.find().sort({ addedAt: 1 }).limit(20).populate("person");
+            return fetchedCards.map(obj => obj.toObject()) as unknown as CardsType;
+        }
+
+    }
 
     useEffect(() => {
         if (cards.length) return;
-
-        const fetchCards = async () => {
-            if (cards.length) {
-
-                const fetchedCards = await Card.collection.find({
-                    _id: {
-                        $gt: cards[cards.length - 1]._id
-                    }
-                }).limit(20);
-
-                return fetchedCards.toArray() as unknown as CardsType;
-
-            }
-            else {
-                const fetchedCards = await Card.find().limit(20).populate("person");
-                return fetchedCards.map(obj => obj.toObject()) as unknown as CardsType;
-            }
-
-        }
 
         fetchCards().then((data) => {
             setCards([...cards, ...data]);
@@ -75,9 +101,33 @@ export default function CardsTable() {
         setCards([card.toObject(), ...cards]);
     }
 
+    useEffect(() => {
+        const listener = () => {
+
+            // if the user scrolled to the bottom of table, fetch more cards
+            if ((window.scrollY + 200) >= ((ref.current?.clientHeight || Infinity) - window.screen.availHeight)) {
+                fetchCards().then((c) => {
+                    setCards([...cards, ...c]);
+                });
+            }
+        };
+
+        window.addEventListener("scroll", listener);
+
+        return () => {
+            window.removeEventListener("scroll", listener);
+        }
+    });
+
+
     return (
-        <div className="">
+        <div className="" ref={ref}>
             <Paper className="p-4 text-center">
+                <div className="flex justify-end">
+                    <Tooltip title={t("card.addCard")}>
+                        <AddCardButton cards={cards as unknown as CardType[]} onAdd={onAddCard} />
+                    </Tooltip>
+                </div>
                 <Table>
                     <TableHead>
                         <TableRow>
@@ -92,13 +142,10 @@ export default function CardsTable() {
                         {cards.map((card) => (
                             <CardRow card={card} onDeleteCard={deleteCard} onChangeCardOwner={updateCardOwner} />
                         ))}
+
+                        <div ref={hiddenRef} className=""></div>
                     </TableBody>
 
-                    <TableFooter>
-                        <Tooltip title={t("card.addCard")}>
-                            <AddCardButton cards={cards as unknown as CardType[]} onAdd={onAddCard} />
-                        </Tooltip>
-                    </TableFooter>
                 </Table>
             </Paper>
 
